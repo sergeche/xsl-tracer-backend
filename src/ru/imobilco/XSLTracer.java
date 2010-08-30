1291fc154f96d302824b6055f06089fed5702f9f
Sergey Chikuyonok <serge.che@gmail.com>
2010-08-31 01:55:50 +0400

    Force transformer output params

--- src/ru/imobilco/XSLTracer.java
+++ src/ru/imobilco/XSLTracer.java
@@ -1,15 +1,22 @@
 package ru.imobilco;
 
 import java.io.File;
+import java.util.Date;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
 
+import com.icl.saxon.Controller;
 import com.icl.saxon.FeatureKeys;
 import com.icl.saxon.ParameterSet;
 import com.icl.saxon.PreparedStyleSheet;
 import com.icl.saxon.StyleSheet;
+import com.icl.saxon.style.TerminationException;
 import com.icl.saxon.trace.TraceListener;
 
 public class XSLTracer extends StyleSheet {
@@ -32,8 +39,31 @@ public class XSLTracer extends StyleSheet {
     	if (tracer != null && tracer instanceof JSONTraceListener) {
     		((JSONTraceListener) tracer).setBaseFiles(source.getSystemId(), 
     				((PreparedStyleSheet) sheet).getStyleSheetDocument().getSystemId());
+    		
+    		
     	}
     	
-    	super.processFile(source, sheet, outputFile, params);
+    	Transformer instance = sheet.newTransformer();
+    	((Controller)instance).setParams(params);
+    	
+    	// force transformer to output XML result
+    	instance.setOutputProperty(OutputKeys.ENCODING, "utf8");
+    	instance.setOutputProperty(OutputKeys.METHOD, "xml");
+    	instance.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+    	instance.setOutputProperty(OutputKeys.INDENT, "no");
+    	
+    	Result result =
+    		(outputFile==null ?
+    				new StreamResult(System.out) :
+    					new StreamResult(outputFile));
+    	
+    	try {
+    		instance.transform(source, result);
+    	} catch (TerminationException err) {
+    		throw err;
+    	} catch (TransformerException err) {
+    		// The message will already have been displayed; don't do it twice
+    		throw new TransformerException("Run-time errors were reported");
+    	}
     }
 }
